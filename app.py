0e4ea456992316d274aa556ae7fc04d564089835
MicAPic <micha.pavlov@gmail.com>
2021-03-29 15:05:37 +0300

    Program now shows the path between two stations

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter import messagebox
 from algorithm import Metro_Dijkstra
 from PIL import Image, ImageTk
 import init
@@ -7,6 +6,7 @@ import init
 
 def enter(e):
     if e.keycode == 2359309:  # keycode for enter on macos
+        reset()
         clicked()
 
 
@@ -15,8 +15,10 @@ def clicked(*args):
     if node.isdigit() and 0 <= int(node) < init.N:
         stations.append(node)
     if len(stations) == 2:
-        messagebox.showinfo('Кратчайший маршрут',
-                            Metro_Dijkstra(init.N, int(stations[0]), int(stations[1]), init.routes))
+        res = Metro_Dijkstra(init.N, int(stations[0]), int(stations[1]), init.routes)
+        result.config(text=f'Кратчайший маршрут: {res[0]} мин')
+        for _ in range(len(res[1])):
+            canvas.itemconfig(f'c_{res[1][_]}', fill='dim gray')
         stations.clear()
     textbox.delete(0, END)
     textbox.insert(0, "")
@@ -25,38 +27,47 @@ def clicked(*args):
 def clicked_node(e=None, num=0):
     textbox.delete(0, END)
     textbox.insert(0, num)
+    reset()
     clicked()
 
 
-def create_circle(x, y, r, num, color, canvas_name):
+def create_circle(x, y, r, num, color):
     transfer_half_list = [26, 43, 44, 50, 52, 64]
     if num in transfer_half_list:
-        canvas_name.create_arc(x - r, y - r, x + r, y + r,
-                               fill=f'{color}',
-                               tags=f'c_{num}',
-                               outline='white',
-                               start=90,
-                               extent=-180)
+        canvas.create_arc(x - r, y - r, x + r, y + r,
+                          fill=f'{color}',
+                          tags=f'c_{num}',
+                          outline='white',
+                          start=90,
+                          extent=-180)
     elif num == 49:
-        canvas_name.create_arc(x - r, y - r, x + r, y + r,
-                               fill=f'{color}',
-                               tags=f'c_{num}',
-                               outline='white',
-                               start=300,
-                               extent=120)
+        canvas.create_arc(x - r, y - r, x + r, y + r,
+                          fill=f'{color}',
+                          tags=f'c_{num}',
+                          outline='white',
+                          start=300,
+                          extent=120)
     elif num == 65:
-        canvas_name.create_arc(x - r, y - r, x + r, y + r,
-                               fill=f'{color}',
-                               tags=f'c_{num}',
-                               outline='white',
-                               start=60,
-                               extent=120)
+        canvas.create_arc(x - r, y - r, x + r, y + r,
+                          fill=f'{color}',
+                          tags=f'c_{num}',
+                          outline='white',
+                          start=60,
+                          extent=120)
     else:
-        canvas_name.create_oval(x - r, y - r, x + r, y + r,
-                                fill=f'{color}',
-                                outline='white',
-                                tags=f'c_{num}',)
+        canvas.create_oval(x - r, y - r, x + r, y + r,
+                           fill=f'{color}',
+                           outline='white',
+                           tags=f'c_{num}', )
     canvas.tag_bind(f'c_{num}', '<Button>', lambda e: clicked_node(e, num))
+
+
+def reset():
+    if not stations:
+        result.config(text='Кратчайший маршрут:')
+        for _ in range(init.N):
+            canvas.itemconfig(f'c_{init.Station.registry[_].id}', fill=f'{init.Station.registry[_].line}')
+    canvas.itemconfig('current', fill='dim gray')
 
 
 stations = []
@@ -73,13 +84,14 @@ canvas.place(x=-5, y=-5)
 metro_map = ImageTk.PhotoImage(Image.open('metro_map.png'))  # source: metro-spb.ru
 img = canvas.create_image(301, 352, image=metro_map)
 
-prompt = Label(window, text='Выберите две станции по ID\nили нажмите на них:', justify=LEFT)
+prompt = Label(window, text='Выберите две станции по ID:     ', justify=LEFT)
 prompt.grid(column=0, row=0, sticky='W')
 textbox = Entry(window, width=10)
 textbox.grid(column=0, row=1, sticky='W', padx='5', ipadx='42')
-# textbox.focus()
 button = Button(window, text="Ввод", command=clicked)
 button.grid(column=0, row=2, sticky='W', padx='5', ipadx='73')
+result = Label(window, text='Кратчайший маршрут:       ', justify=LEFT)
+result.grid(column=0, row=3, sticky='W')
 
 for station in init.Station.registry:
     if station.coordinates is not None:
@@ -87,6 +99,6 @@ for station in init.Station.registry:
             radius = 9
         else:
             radius = 6
-        create_circle(station.coordinates[0], station.coordinates[1], radius, station.id, station.line, canvas)
+        create_circle(station.coordinates[0], station.coordinates[1], radius, station.id, station.line)
 
 window.mainloop()
